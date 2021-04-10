1900cf97eab16ef4d3a0d5db9c661087aef428cb
yongzhe.dong <yongzhe.dong@joymo.tech>
2021-04-10 14:56:21 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -10,21 +10,21 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
+echo 'doc.dyzzz.top' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:dongyzzzz/Study-Notes.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://dongyzzzz:${GITHUB_TOKEN}@github.com/dongyzzzz/Study-Notes.git
+  git config --global user.name "dyzzz"
+  git config --global user.email "58596779@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl master
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
