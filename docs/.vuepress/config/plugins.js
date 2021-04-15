5f06394c2cd7f0924764acf88691bc053f4ddfe7
yongzhe.dong <yongzhe.dong@joymo.tech>
2021-04-15 17:48:49 +0800

    评论配置

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -83,11 +83,11 @@ module.exports = [
     {
       choosen: 'gitalk',
       options: {
-        clientID: 'a6e1355287947096b88b',
-        clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'xugaoyi', // GitHub仓库所有者
-        admin: ['xugaoyi'], // 对仓库有写权限的人
+        clientID: '446ded0cdb2e15c9f37e',
+        clientSecret: '1b46f29ce1c47a21e95dc3376a98b9509dfe4335',
+          repo: 'Study-Notes', // GitHub 仓库
+        owner: 'dongyzzzz', // GitHub仓库所有者
+        admin: ['dongyzzzz'], // 对仓库有写权限的人
         // distractionFreeMode: true,
         pagerDirection: 'last', // 'first'正序 | 'last'倒序
         id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
